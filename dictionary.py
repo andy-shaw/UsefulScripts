f13db10d83f1564a04e87154ffcc1c7650291114
Andy Shaw <c00lnerd314@gmail.com>
2014-08-21 14:16:09 -0400

    add indexed list to dictionary module

--- dictionary.py
+++ dictionary.py
@@ -8,6 +8,7 @@ This module provides a dictionary to interact with.
 default_name = 'enable1.txt'
 path = r'C:\\Python27\\Lib\\'
 
+import string
 from string import lower
 
 def fetch(length=None):
@@ -33,3 +34,21 @@ def fetch(length=None):
                 words.append(word)
             word = f.readline().strip()
     return words
+
+def fetch_indexed():
+    '''return a tree of all the words for quick indexing'''
+    f = open(path+default_name, 'r')
+    words = {}
+    for s in string.lowercase:
+        words[s] = []
+
+    if raw_input('The dict is large, do you wish to load? (y/n)').lower() == 'y':
+        word = f.readline().strip()
+        while word:
+            #append words that start with certain letter to that letter in the dictionary
+            words[word[0]].append(word)
+            word = f.readline().strip()
+        return words
+
+    else:
+        return {}
