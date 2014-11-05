03d839be4625f2f8391858427abaf44da29e6e50
c00lnerd314 <c00lnerd314@gmail.com>
2014-11-04 21:49:32 -0800

    installation paths are not relative anymore

--- install_modules.bat
+++ install_modules.bat
@@ -2,9 +2,9 @@
 
 echo Installing dictionary module to python 2.7 folder
 
-xcopy enable1.txt C:\Python27\Lib\ /Y /F
-xcopy dictionary.py C:\Python27\Lib\ /Y /F
+xcopy C:\UsefulScripts\enable1.txt C:\Python27\Lib\ /Y /F
+xcopy C:\UsefulScripts\dictionary.py C:\Python27\Lib\ /Y /F
 
-xcopy compress.py C:\Python27\Lib\ /Y /F
+xcopy C:\UsefulScripts\compress.py C:\Python27\Lib\ /Y /F
 
 echo on
