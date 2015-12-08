b2ec5a916422f872cf1c90f04a9ad10ac4ad6021
Andy <c00lnerd314@gmail.com>
2015-12-07 23:44:50 -0800

    implement workdays calculator for budget planning

--- budget.py
+++ budget.py
@@ -1,2 +1,30 @@
-import math
+import datetime
 
+def workdays(month, year):
+    months = {
+        'jan': 1,
+        'feb': 2,
+        'mar': 3,
+        'april': 4,
+        'may': 5,
+        'june': 6,
+        'july': 7,
+        'aug': 8,
+        'sept': 9,
+        'oct': 10,
+        'nov': 11,
+        'dec': 12
+    }
+
+    month = months[month.lower()]
+    next_month = (month + 1) % (len(months.keys()) + 1)
+    year = int(year)
+    if month > next_month: 
+        next_year = int(year) + 1
+    else:
+        next_year = year
+
+    fromdate = datetime.date(year,month,1)
+    todate = datetime.date(next_year,next_month,1)
+    daygenerator = (fromdate + datetime.timedelta(x + 1) for x in xrange((todate - fromdate).days))
+    return sum(1 for day in daygenerator if day.weekday() < 5)
